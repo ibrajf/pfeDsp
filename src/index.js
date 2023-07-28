5b4c6e8472dc68be79b3ced191be5e0d3ffb303b
JEFFALI <i.jeffali43@gmail.com>
2023-07-28 15:15:54 +0200

    add the adding code modal

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Blog from "./components/Blog"
 import Historique from "./components/Historique"
 import Blogdetails from "./components/details/Blogdetails"
 import CheckCode from "./components/CheckCode"
+import Addcode from "./components/AddCode"
 import reportWebVitals from "./reportWebVitals"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Faq from "./components/Faq"
@@ -24,6 +25,7 @@ ReactDOM.render(
             <Route path="/faq" element={<Faq />} />
             <Route path="/historique" element={<Historique />} />
             <Route path="/blog/detail/:id" element={<Blogdetails />} />
+            <Route path="/Addcode" element={<Addcode />} />
             <Route path="/" element={<CheckCode />} />
           </Routes>
         </Layout>
