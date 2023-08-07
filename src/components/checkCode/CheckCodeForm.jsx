20a9de2394ea858e13c152bedeeae010448913aa
Ibrahim JEFFALI <ibrahim@euthenia-groupe.fr>
2023-08-07 16:47:48 +0200

    add the content and fix responsive in checkcode page

--- src/components/checkCode/CheckCodeForm.jsx
+++ src/components/checkCode/CheckCodeForm.jsx
@@ -22,12 +22,13 @@ const CheckCodeForm = () => {
   }
 
   const renderAuthForm = () => (
-    <Flex direction={{ base: "column", sm: "row" }} spacing={{ base: "4", sm: "8" }} w="100%">
-      <SignIn />
-      <SignUp />
-    </Flex>
+    <Box overflow="auto">
+      <Flex direction={{ base: "column", sm: "row" }} spacing={{ base: "4", sm: "8" }} w="100%">
+        <SignIn />
+        <SignUp />
+      </Flex>
+    </Box>
   )
-
   const renderCodeForm = () => (
     <>
       <Box display="flex" justifyContent="center">
@@ -48,7 +49,7 @@ const CheckCodeForm = () => {
   )
 
   return (
-    <Box p={4} overflow="hidden" alignSelf={'center'} justifySelf={'center'} justifyContent={'center'} display="flex" flexDirection="column" bg={useColorModeValue("#FFFAF0", "gray.900")} w={{ base: "80%", sm: "45%" }} h='92%' ml={{ base: "0", sm: "35%" }} boxShadow={'xl'} borderRadius={10} >
+    <Box p={4} overflow="hidden" alignSelf={'center'} justifySelf={'center'} justifyContent={'center'} display="flex" flexDirection="column" bg={useColorModeValue("#FFFAF0", "gray.900")} w={{ base: "80%",  md: "80%" }} h='92%' ml={{ base: "0",lg: '20%', md: "35%" }} boxShadow={'xl'} borderRadius={10} >
       {/* <Flex justifyContent="center">
         <Box>
           <Image w="75%" src={logo} alt="Logo Thetiptop" />
--- src/components/login/SignUp.jsx
+++ src/components/login/SignUp.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useState } from "react"
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react"
+import { Button, FormControl, FormLabel, Input, Heading, Box } from "@chakra-ui/react"
 
 function SignUp() {
   const [formValues, setFormValues] = useState({
@@ -25,25 +25,28 @@ function SignUp() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl>
-        <FormLabel>First Name</FormLabel>
-        <Input type="text" placeholder="Enter your first name" onChange={e => setFormValues({ ...formValues, firstName: e.target.value })} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Last Name</FormLabel>
-        <Input type="text" placeholder="Enter your last name" onChange={e => setFormValues({ ...formValues, lastName: e.target.value })} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Email</FormLabel>
-        <Input type="email" placeholder="Enter your email" onChange={e => setFormValues({ ...formValues, email: e.target.value })} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" placeholder="Enter a strong password" onChange={e => setFormValues({ ...formValues, password: e.target.value })} />
-      </FormControl>
-      <Button type="submit">Sign Up</Button>
-    </form>
+    <Box maxWidth="350px" mx="auto" mt="20px">
+      <Heading marginBottom="20px">Inscription</Heading>
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <FormLabel>Prénom</FormLabel>
+          <Input type="text" placeholder="Entrez votre prénom" onChange={e => setFormValues({ ...formValues, firstName: e.target.value })} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Nom</FormLabel>
+          <Input type="text" placeholder="Entrez votre nom" onChange={e => setFormValues({ ...formValues, lastName: e.target.value })} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" placeholder="Entrez votre email" onChange={e => setFormValues({ ...formValues, email: e.target.value })} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Mot de passe</FormLabel>
+          <Input type="password" placeholder="Entrez un mot de passe puissant" onChange={e => setFormValues({ ...formValues, password: e.target.value })} />
+        </FormControl>
+        <Button my={5} w='100%' type="submit">S'inscrire</Button>
+      </form>
+    </Box>
   )
 }
 
