635088c824de9accb1338b058699afde388e50ef
Ibrahim JEFFALI <ibrahim@euthenia-groupe.fr>
2023-08-07 14:46:23 +0200

    add the content to home page and checkcode page

--- src/components/Home/Home_features.jsx
+++ src/components/Home/Home_features.jsx
@@ -31,23 +31,23 @@ export default function SimpleThreeColumns() {
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
         <Feature
           icon={<Icon as={FcAssistant} w={10} h={10} />}
-          title={'Lifetime Support'}
+          title={'Jouez et Gagnez avec ThéTipTop!'}
           text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+            'Participez à notre jeu-concours unique en son genre pour célébrer l`ouverture de notre 10ème boutique à Nice. Tous les clients avec un ticket de caisse de plus de 49€ recevront un numéro à 10 chiffres pour une participation assurée. Des récompenses théinées sont à gagner à 100% : des infuseurs, des thés détox, des thés signatures, et des coffrets découverte.'
           }
         />
         <Feature
           icon={<Icon as={FcDonate} w={10} h={10} />}
-          title={'Unlimited Donations'}
+          title={'Découvrez les Saveurs Uniques du Thé avec ThéTipTop!'}
           text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+            'Explorez notre sélection exquise de thés bios, faits à la main, et laissez-vous tenter par notre jeu-concours exceptionnel. Gagnez des infuseurs, des thés détox, des thés signatures, et des coffrets découverte d`une valeur allant jusqu`à 69€. Tentez votre chance dès maintenant!'
           }
         />
         <Feature
           icon={<Icon as={FcInTransit} w={10} h={10} />}
-          title={'Instant Delivery'}
+          title={'Un An de Thé en Jeu!'}
           text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+            'Participez au jeu-concours ThéTipTop et tentez de gagner un an de thé d`une valeur de 360€. Profitez également de 30 jours pour tester vos numéros de ticket et réclamer vos récompenses en magasin ou en ligne. Une expérience théinée unique vous attend !'
           }
         />
       </SimpleGrid>
