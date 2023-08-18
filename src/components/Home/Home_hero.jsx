f3feb63a46ef48a68f0958f68b5cc36be42dab98
Ibrahim JEFFALI <ibrahim@euthenia-groupe.fr>
2023-08-18 14:39:49 +0200

    test auto pull to main

--- src/components/Home/Home_hero.jsx
+++ src/components/Home/Home_hero.jsx
@@ -30,7 +30,7 @@ export default function WithBackgroundImage() {
             lineHeight={1.2}
             align={'center'}
             fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
-            Jouez pour découvrir des arômes inattendus et des récompenses gourmandes!
+            Jouez pour découvrir des arômes inattendus et des récompenses gourmandes! tester l'automatisation du pull
           </Text>
           <Stack direction={'row'}>
             <Link href='/checkcode'>
