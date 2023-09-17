4995a9644c359fd92c05bc0dd42d74227370f65e
AmzylMohcine <amzylmohcine@gmail.com>
2023-09-17 22:14:09 +0200

    icons DONE

--- src/components/Home/Home_features.jsx
+++ src/components/Home/Home_features.jsx
@@ -1,5 +1,5 @@
 import { Box, SimpleGrid, Icon, Text, Stack, Flex, Heading } from "@chakra-ui/react"
-import { FcAssistant, FcDonate, FcInTransit } from "react-icons/fc"
+import { FcApproval, FcBinoculars, FcMoneyTransfer } from "react-icons/fc"
 
 const Feature = ({ title, text, icon }) => {
   return (
@@ -20,9 +20,9 @@ export default function SimpleThreeColumns() {
         <Heading aria-label="Section title">Qui sommes nous ?</Heading>
       </Stack>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} aria-label="Features grid">
-        <Feature icon={FcAssistant} title={"Jouez et Gagnez avec ThéTipTop!"} text={"Participez à notre jeu-concours unique en son genre pour célébrer l`ouverture de notre 10ème boutique à Nice. Tous les clients avec un ticket de caisse de plus de 49€ recevront un numéro à 10 chiffres pour une participation assurée. Des récompenses théinées sont à gagner à 100% : des infuseurs, des thés détox, des thés signatures, et des coffrets découverte."} />
-        <Feature icon={FcDonate} title={"Découvrez les Saveurs Uniques du Thé avec ThéTipTop!"} text={"Explorez notre sélection exquise de thés bios, faits à la main, et laissez-vous tenter par notre jeu-concours exceptionnel. Gagnez des infuseurs, des thés détox, des thés signatures, et des coffrets découverte d`une valeur allant jusqu`à 69€. Tentez votre chance dès maintenant!"} />
-        <Feature icon={FcInTransit} title={"Un An de Thé en Jeu!"} text={"Participez au jeu-concours ThéTipTop et tentez de gagner un an de thé d`une valeur de 360€. Profitez également de 30 jours pour tester vos numéros de ticket et réclamer vos récompenses en magasin ou en ligne. Une expérience théinée unique vous attend !"} />
+        <Feature icon={FcApproval} title={"Jouez et Gagnez avec ThéTipTop!"} text={"Participez à notre jeu-concours unique en son genre pour célébrer l`ouverture de notre 10ème boutique à Nice. Tous les clients avec un ticket de caisse de plus de 49€ recevront un numéro à 10 chiffres pour une participation assurée. Des récompenses théinées sont à gagner à 100% : des infuseurs, des thés détox, des thés signatures, et des coffrets découverte."} />
+        <Feature icon={FcBinoculars} title={"Découvrez les Saveurs Uniques du Thé avec ThéTipTop!"} text={"Explorez notre sélection exquise de thés bios, faits à la main, et laissez-vous tenter par notre jeu-concours exceptionnel. Gagnez des infuseurs, des thés détox, des thés signatures, et des coffrets découverte d`une valeur allant jusqu`à 69€. Tentez votre chance dès maintenant!"} />
+        <Feature icon={FcMoneyTransfer} title={"Un An de Thé en Jeu!"} text={"Participez au jeu-concours ThéTipTop et tentez de gagner un an de thé d`une valeur de 360€. Profitez également de 30 jours pour tester vos numéros de ticket et réclamer vos récompenses en magasin ou en ligne. Une expérience théinée unique vous attend !"} />
       </SimpleGrid>
     </Box>
   )
