9f4cb2a3d1493830cd952f40aa3cdc4bbcae2d9d
Ibrahim JEFFALI <ibrahim@euthenia-groupe.fr>
2023-09-18 12:34:01 +0200

    add images and titles to blog

--- src/services/articles.js
+++ src/services/articles.js
@@ -1,26 +1,26 @@
 export const articles = [
   {
     id: 1,
-    title: "How to participate ?",
-    image: "https://placehold.co/600x400/png",
+    title: "Comment participer ?",
+    image: "https://images.pexels.com/photos/3643455/pexels-photo-3643455.jpeg?auto=compress&cs=tinysrgb&w=600",
     content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
   {
     id: 2,
-    title: "How to use the code ?",
-    image: "https://placehold.co/600x400/png",
+    title: "comment utiliser le code?",
+    image: "https://images.pexels.com/photos/9022942/pexels-photo-9022942.jpeg?auto=compress&cs=tinysrgb&w=800",
     content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
   {
     id: 3,
-    title: "Where to get the prise ?",
-    image: "https://placehold.co/600x400/png",
+    title: "Récuperer le prix",
+    image: "https://images.pexels.com/photos/16716139/pexels-photo-16716139/free-photo-of-nourriture-bois-tasse-grande-tasse.jpeg?auto=compress&cs=tinysrgb&w=800",
     content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
   {
     id: 4,
-    title: "What is the big prise ?",
-    image: "https://placehold.co/600x400/png",
+    title: "Grand tombola",
+    image: "https://images.pexels.com/photos/15105318/pexels-photo-15105318/free-photo-of-restaurant-mode-homme-gens.jpeg?auto=compress&cs=tinysrgb&w=800",
     content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   }
 ]
